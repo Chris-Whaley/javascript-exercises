2f8030d1a5816066f6ce7877ec597345acd82499
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-01 15:11:53 -0500

    function will now take indefinite number of items to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,26 @@
-const removeFromArray = function(inArray, removeItems) {
+const removeFromArray = function(inArray, ...removeAdditionalItems) {
 
-    let outArray = [];
+    let outArray = inArray;
 
-    // loop through the input array
-    for (const item of inArray) {
-        if (item != removeItems) {
-            outArray.push(item);
+    // outer loop through items to remove
+    for (const removalItem of removeAdditionalItems){
+        for (let index = 0; index < inArray.length; index++) {
+            if(outArray[index] == removalItem) {
+                delete outArray[index];
+            }
+            
         }
     }
+
+    // filter out the undefined caused by the delete method in loop above
+    outArray = outArray.filter(function(element) {
+        return element != undefined;
+    });
 
     return outArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+removeFromArray([1, 2, 3, 4], 3, 2)
