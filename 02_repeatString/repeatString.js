ff76bee765b49f70460cf0a278701f2879bd4058
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-01 13:24:37 -0500

    fix: Add loop and exclusion for number parameter

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(inString, inNum) {
+    let outString = '';
 
+    if (inNum < 0) {
+        return 'ERROR';
+    } else {
+        for (let index = 0; index < inNum; index++) {
+            outString += inString;        
+        }
+    }
+
+    return outString;
 };
 
 // Do not edit below this line
