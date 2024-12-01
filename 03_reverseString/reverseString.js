ad1be196e45045f032036806349bb27a292095e7
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-01 13:38:24 -0500

    fix: Initial loop backwards through string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inString = '') {
+    let outString = '';
 
+    for (let index = inString.length - 1; index >= 0; --index) {
+        outString += inString[index];
+        
+    }
+
+    return outString;
 };
 
 // Do not edit below this line
