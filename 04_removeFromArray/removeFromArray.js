93cc3a927f853c0b1e066e2eaa86a4d0742be11e
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-01 14:26:21 -0500

    Initial check for one item to remove

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inArray, removeItems) {
+
+    let outArray = [];
+
+    // loop through the input array
+    for (const item of inArray) {
+        if (item != removeItems) {
+            outArray.push(item);
+        }
+    }
+
+    return outArray;
 };
 
 // Do not edit below this line
