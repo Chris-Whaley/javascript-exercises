b96543ff9cc1df82c177f11e13bca51bde39510a
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 13:28:40 -0500

    complete: Solve lesson 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
+
+};
+
+const factorial = function(a) {
+  
+  let allNums = [];
+
+  // get all numbers up until factorial request (i.e., "a")
+  for (let index = 1; index <= a; index++) {
+    allNums.splice(index, 0, index);
+    
+  }
+  
+  let result = allNums.reduce((total, current) => total * current, 1);
+
+  return result;
 	
 };
 
-const factorial = function() {
-	
-};
+
 
 // Do not edit below this line
 module.exports = {
@@ -31,3 +50,4 @@ module.exports = {
   power,
   factorial
 };
+
