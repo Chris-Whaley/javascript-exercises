a8ce1af9e548a694f2981863eb374953010a1cfd
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 15:01:24 -0500

    solved: Remove non-alphanumeric characters and compare arrays

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    
+    let inputText = input.toLowerCase();
 
+    inputText = inputText.replace(/[^a-z0-9]/gi, "");
+    
+    let inputArr = Array.from(inputText.split(''));
+    let reverseArr = Array.from(inputText.split(''));
+
+    reverseArr.reverse();
+    let reverseText = '';
+
+    
+    // join back into string
+    reverseArr.forEach(element => {
+        reverseText += element;
+    });;
+    
+    if (inputText.toLowerCase() == reverseText.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+
+    
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
