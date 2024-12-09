e9f47cf87e45114c95774f9d17e1e9056e8e5358
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 20:37:50 -0500

    solved: Add logic to check if a key exists

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
 const findTheOldest = function(obj) {
 
-    const oldest = obj.sort((personA, personB) => {
+    let inputObject = obj;
+
+    // loop through object adding current year if no death date
+    inputObject.map((person) => {
+        if (person.hasOwnProperty('yearOfDeath')) {
+            person.yearOfDeath = person.yearOfDeath;
+        } else {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    })
+
+    const oldest = inputObject.sort((personA, personB) => {
         const youngerPerson = personA.yearOfDeath - personA.yearOfBirth;
         const olderPerson = personB.yearOfDeath - personB.yearOfBirth;
 
@@ -12,24 +23,23 @@ const findTheOldest = function(obj) {
 
 };
 
-// const people = [
-//     {
-//       name: "Carly",
-//       yearOfBirth: 1942,
-//       yearOfDeath: 1970,
-//     },
-//     {
-//       name: "Ray",
-//       yearOfBirth: 1962,
-//       yearOfDeath: 2011,
-//     },
-//     {
-//       name: "Jane",
-//       yearOfBirth: 1912,
-//       yearOfDeath: 1941,
-//     },
-//   ];
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1000,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
 
-// console.log (findTheOldest(people) );
+console.log (findTheOldest(people) );
 // Do not edit below this line
 module.exports = findTheOldest;
