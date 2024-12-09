210bc31319498d572c6c8cc15f60b5d212420203
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 20:15:53 -0500

    fix: Compare each person and return value in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+
+    const oldest = obj.sort((personA, personB) => {
+        const youngerPerson = personA.yearOfDeath - personA.yearOfBirth;
+        const olderPerson = personB.yearOfDeath - personB.yearOfBirth;
+
+        return youngerPerson < olderPerson ? 1 : - 1;
+
+    });
+
+    return oldest[0];
 
 };
 
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ];
+
+// console.log (findTheOldest(people) );
 // Do not edit below this line
 module.exports = findTheOldest;
