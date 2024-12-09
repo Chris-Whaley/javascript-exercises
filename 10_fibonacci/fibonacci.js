68a42aaf69c68901095e8d8f53ba166a7119707b
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 19:53:10 -0500

    fix: Add initial array and return correct index

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    arr = [1, 1, ];
+    // example: 1, 1, 2, 3, 5, 8
+    // need 2 numbers. first = n - 2
+    // second = n - 1
+    // calc: (n - 2) + (n - 1)
+    for (let index = 2; index <= num; index++) {
+        let element = arr[index - 2] + arr[index - 1];
+        arr[index] = element;
+    }
+
+    return arr[num - 1];
 };
-
+// console.log( fibonacci(5) );
 // Do not edit below this line
 module.exports = fibonacci;
