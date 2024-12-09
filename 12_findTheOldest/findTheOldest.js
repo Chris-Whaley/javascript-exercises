ff543f3cfe2667a9fb5d7ca8c0a8a9e6f3648454
Chris-Whaley <27449071+Chris-Whaley@users.noreply.github.com>
2024-12-08 20:39:37 -0500

    solved: Add logic to check for death date

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -23,23 +23,23 @@ const findTheOldest = function(obj) {
 
 };
 
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1000,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ];
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1000,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ];
 
-console.log (findTheOldest(people) );
+// console.log (findTheOldest(people) );
 // Do not edit below this line
 module.exports = findTheOldest;
